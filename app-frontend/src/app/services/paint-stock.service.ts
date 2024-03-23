c318b06b6dd17b879ebecb8485e09d67f5cdc536
Matt Marko <mattmarko321@gmail.com>
2024-03-23 12:04:30 -0700

    change http service

--- app-frontend/src/app/services/paint-stock.service.ts
+++ app-frontend/src/app/services/paint-stock.service.ts
@@ -4,11 +4,13 @@ import { Colour } from '../enums/colour';
 import { Status } from '../enums/status';
 import { Paint } from '../paint';
 import { Observable, map } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaintStockService {
+  apiUrl: string = environment.apiUrl;
 
   private http: HttpClient = inject(HttpClient);
 
@@ -38,10 +40,10 @@ export class PaintStockService {
   }
 
   getPaints(): Observable<Paint[]> {
-    return this.http.get<Paint[]>('http://localhost:8080/paint');
+    return this.http.get<Paint[]>(this.apiUrl);
   }
 
   updatePaints(paints: Paint[]): Observable<Paint[]> {
-    return this.http.put<Paint[]>('http://localhost:8080/paint', paints);
+    return this.http.put<Paint[]>(this.apiUrl, paints);
   }
 }
